8b9d52c9604628e878e06f2e67fd17f290e88185
Alberto <albertomaronezio@hotmai.com>
2019-05-31 17:02:48 -0300

    Calcular_Imposto_De_Renda

---
+++ Calculadora_Imposto_De_Renda/Calculadora_Imposto_De_Renda/Calculo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Imposto_De_Renda
+{
+    public class Calculo
+    {
+        public double rendimentoPoupanca { get; set; }
+        public double rendimentoRendaFixa { get; set; }
+
+        public void RendimentoPoupanca(int meses, double valor)
+        {
+            double rendimentoTotal = 0;
+
+            for (int i = 1; i < meses; i++)
+            {
+                if (i == 1)
+                {
+                    rendimentoPoupanca = (valor * 0.0066) + valor;
+                }
+
+                else
+                {
+                    rendimentoPoupanca = (rendimentoPoupanca * 0.0066) + rendimentoPoupanca;
+                }
+            }
+
+            rendimentoTotal = rendimentoPoupanca - valor;
+
+            Informacao infor = new Informacao();
+            infor.InformarRendimentoPoupanca(meses, rendimentoPoupanca, rendimentoTotal);
+        }
+
+        public void RendimentoRendaFixa(int meses, double valor)
+        {
+            double rendimentoTotal = 0;
+            double imposto = 0;
+
+            for (int i = 1; i < meses; i++)
+            {
+                if (i == 1)
+                {
+                    rendimentoRendaFixa = (valor * 0.0098) + valor;
+                }
+
+                else
+                {
+                    rendimentoRendaFixa = (rendimentoRendaFixa * 0.0098) + rendimentoRendaFixa;
+                }
+            }
+
+            rendimentoTotal = rendimentoRendaFixa - valor;
+
+            if (meses <= 12)
+            {
+                imposto = rendimentoTotal * 0.0025;
+                rendimentoTotal -= imposto;
+            }
+
+            else if (meses > 12 && meses <= 24)
+            {
+                imposto = rendimentoTotal * 0.0015;
+                rendimentoTotal -= imposto;
+            }
+
+            else if (meses > 24 && meses <= 36)
+            {
+                imposto = rendimentoTotal * 0.0005;
+                rendimentoTotal -= imposto;
+            }
+
+            else
+            {
+                imposto = rendimentoTotal * 0.0001;
+                rendimentoTotal -= imposto;
+            }
+
+            rendimentoRendaFixa -= imposto;
+
+            Informacao infor = new Informacao();
+            infor.InformarRendimentoRendaFixa(meses, rendimentoRendaFixa, rendimentoTotal, imposto);
+        }
+
+        public void MelhorInvestimento()
+        {
+            Informacao infor = new Informacao();
+
+            if (rendimentoPoupanca > rendimentoRendaFixa)
+            {
+                infor.MelhorRendimento("Poupança");
+            }
+
+            else
+            {
+                infor.MelhorRendimento("Renda Fixa");
+            }
+        }
+    }
+}
---
+++ Calculadora_Imposto_De_Renda/Calculadora_Imposto_De_Renda/Informacao.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Imposto_De_Renda
+{
+    public class Informacao
+    {
+        public void InformarRendimentoPoupanca(int meses, double valor, double rendimento)
+        {
+            int mesesTotal = meses;
+            double valorTotal = valor;
+            string mesPlural = string.Empty;
+            double valorRendimento = rendimento;
+
+            if (meses == 1)
+            {
+                mesPlural = "Mês";
+            }
+
+            else
+            {
+                mesPlural = "Meses";
+            }
+
+            Console.WriteLine("O Valor total do redimento da Poupança será de: {0} no período de: {1} {2} seu saldo será de: {3}", valorRendimento.ToString("c"), mesesTotal, mesPlural, valorTotal.ToString("c"));
+            Console.ReadKey();
+        }
+
+        public void InformarRendimentoRendaFixa(int meses, double valor, double rendimento, double imposto)
+        {
+            int mesesTotal = meses;
+            double valorTotal = valor;
+            string mesPlural = string.Empty;
+            double valorRendimento = rendimento;
+            double valorImposto = imposto;
+
+            if (meses == 1)
+            {
+                mesPlural = "Mês";
+            }
+
+            else
+            {
+                mesPlural = "Meses";
+            }
+
+            Console.WriteLine("O Valor do redimento da Renda Fixa será de: {0}, Imposto de Renda a ser descontado {1} no período de: {2} {3}, seu saldo será de: {4}", valorRendimento.ToString("c"), valorImposto.ToString("c"), mesesTotal, mesPlural, valorTotal.ToString("c"));
+            Console.ReadKey();
+        }
+
+        public void MelhorRendimento(string melhorInvestimento)
+        {
+            Console.Write("O rendimento mais vantajoso é {0}.", melhorInvestimento);
+            Console.ReadKey();
+        }
+    }
+}
